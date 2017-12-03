cc3707a8fe4e814984b224acb47d195966deb08d
Roman C <myownlab@gmail.com>
2017-12-03 20:09:03 +0000

    update

--- bars.py
+++ bars.py
@@ -4,27 +4,26 @@ from math import radians, cos, sin, asin, sqrt
 
 
 def load_data(filepath):
-    with open(filepath) as data:
-        json_data = json.load(data)
+    with open(filepath) as file:
+        json_data = json.load(file)
     return json_data
 
 
-def get_biggest_bar(bars_data):
-    bars = bars_data['features']
+def get_biggest_bar(bars):
     bar = max(bars, key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
-    return bar['properties']['Attributes']['Name'], bar['properties']['Attributes']['SeatsCount']
+    return bar
 
 
-def get_smallest_bar(bars_data):
-    bars = bars_data['features']
+def get_smallest_bar(bars):
     bar = min(bars, key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
-    return bar['properties']['Attributes']['Name'], bar['properties']['Attributes']['SeatsCount']
+    return bar
 
 
 def get_distance_in_km(lon1, lat1, lon2, lat2):
     """
     Calculate the great circle distance between two points
     on the earth (specified in decimal degrees)
+    from https://stackoverflow.com/a/4913653
     """
     # convert decimal degrees to radians
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
@@ -33,24 +32,28 @@ def get_distance_in_km(lon1, lat1, lon2, lat2):
     dlat = lat2 - lat1
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a))
-    # Radius of earth in kilometers is 6371
-    distance_in_km = 6371 * c
+    radius_of_earth_in_km = 6371
+    distance_in_km = radius_of_earth_in_km * c
     return distance_in_km
 
 
-def get_closest_bar(loaded_data, user_lon, user_lat):
-    bars = bars_data['features']
-    bar = min(bars, key=lambda bar: get_distance_in_km(
-        user_lon, user_lat, *bar['geometry']['coordinates']))
+def get_bar_name(bar):
     return bar['properties']['Attributes']['Name']
 
 
+def get_closest_bar(bars, user_lon, user_lat):
+    bar = min(bars, key=lambda bar: get_distance_in_km(
+        user_lon, user_lat, *bar['geometry']['coordinates']))
+    return bar
+
+
 if __name__ == '__main__':
-    filepath = input("Please provide path to json: ")
+    if len(sys.argv) == 1:
+        sys.exit("Usage: python3 bars.py <path_to_json>")
+    filepath = sys.argv[1]
     bars_data = load_data(filepath)
-    # get_biggest_bar(bars_data)
-    # get_smallest_bar(bars_data)
-    print('The biggest bar is:', get_biggest_bar(bars_data))
-    print('The smallest bar is:', get_smallest_bar(bars_data))
+    bars = bars_data['features']
+    print('The biggest bar is:', get_bar_name(get_biggest_bar(bars)))
+    print('The smallest bar is:', get_bar_name(get_smallest_bar(bars)))
     user_lon, user_lat = input('\nPlease provide your lat long: ').split(" ")
-    print('The closest bar is:', str(get_closest_bar(bars_data, float(user_lon), float(user_lat))))
+    print('The closest bar is:', get_bar_name(get_closest_bar(bars, float(user_lon), float(user_lat))))

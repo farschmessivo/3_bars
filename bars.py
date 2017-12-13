16728289e177a9fd3312f00fe0bce2a08b56f4b7
Roman Ch <hello@romangagarin.com>
2017-12-13 12:11:31 +0000

    changed the comments

--- bars.py
+++ bars.py
@@ -25,9 +25,8 @@ def get_distance_in_km(lon1, lat1, lon2, lat2):
     on the earth (specified in decimal degrees)
     from https://stackoverflow.com/a/4913653
     """
-    # convert decimal degrees to radians
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    # haversine formula
+
     dlon = lon2 - lon1
     dlat = lat2 - lat1
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
